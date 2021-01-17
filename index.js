cea9541f68db7ddcad1871e6b71905c15299bfd4
JT <jt.infiniteio@gmail.com>
2021-01-17 22:59:13 +0000

    removed streams for now as complicating things. Added JSON-RPC as message format

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { node } from './lib/base-node.js'
 
 
 const n = new node()
+n.listen()
 const message = Buffer.from('Some bytes');
 
-n.sendMessage(message, { port: 7301, address: "localhost" })
+n.sendMessage(message, { port: 8089, address: "192.168.0.14" })
