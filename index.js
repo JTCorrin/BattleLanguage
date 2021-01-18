d8f1f569da38badea5bd77bdefcf7d70a493ad5d
JT <jt.infiniteio@gmail.com>
2021-01-18 11:49:49 +0000

    added mssage received event

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import KademliaNode from './lib/kademlia-node.js'
 
 
-const n = new KademliaNode()
-n.listen()
-const message = Buffer.from('Some bytes');
+const n = new KademliaNode(7314)
+// const message = Buffer.from('Some bytes');
 
 let note = n.createNotification('ping', { "test": "hello" })
-console.log(note);
+n.send(note, { address: "", port: "" })
+// console.log(note);
